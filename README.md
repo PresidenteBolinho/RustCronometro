ea32b72fa360c03f19627438b8d969dc0d928b25
Gustavo Pinheiro <gustavospr@hotmail.com>
2022-07-20 07:10:20 -0300

    Considerações iniciais sobre o projeto

---
+++ README.md
@@ -0,0 +1,7 @@
+# Objetivo
+
+Este projeto tem o objetivo de praticar e desenvolver os conhecimentos de programação com Rust, por meio de uma aplicação de um cronometro.
+
+# O que fazer
+
+A lista de coisas sobre o que fazer estão no momento, comentadas no código.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    //Perguntas: Do que eu preciso
+    //Descobrir uma forma de contar o tempo, e retornar essa contagem em tempo real para o contador
+    //Especificiar os comportamentos dos botões e suas interações com o contador
+    println!("Bem vindo ao meu cronometro!");
+
+    //Display que vai mostrar a contagem do tempo
+    println!("Contador"):
+
+    //Localização dos botões ou funções deles
+    println!("Começar");
+    println!("Pausar");
+    println!("Parar");
 }
