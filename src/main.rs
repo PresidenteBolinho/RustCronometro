5cb02183bbed0415dfad4250f8a2837341cbcf5c
Gustavo Pinheiro <gustavospr@hotmail.com>
2022-07-21 12:34:14 -0300

    Criação da função press que representa o aperto de um botão.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
+// Função com o objetivo de simular o aperto de um botão
+fn press() {
+    //Criação da variavel mutavel que vai receber as ações dos botões.
+    let mut action = String::new();
+
+    //Leitura da ação.
+    io::stdin()
+        .read_line(&mut action).
+        expect("Não deu certo.");
+    
+    //Coversão para inteiro.
+    let action: u8 = action.trim().parse().expect("Não é uma opção valida.");
+
+    //Controle de fluxo com base na ação.
+    if action == 1 {
+            println!("Botão apertado");
+    }
+}
+
 fn main() {
     //Perguntas: Do que eu preciso
     //Descobrir uma forma de contar o tempo, e retornar essa contagem em tempo real para o contador
@@ -5,10 +26,12 @@ fn main() {
     println!("Bem vindo ao meu cronometro!");
 
     //Display que vai mostrar a contagem do tempo
-    println!("Contador"):
+    println!("Contador");
 
     //Localização dos botões ou funções deles
     println!("Começar");
     println!("Pausar");
     println!("Parar");
+
+    press();
 }
